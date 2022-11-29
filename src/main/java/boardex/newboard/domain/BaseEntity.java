041aab713e2530ad047128610917fed69d1fef8e
Choi jun hyoek <chlwnsgur1214@naver.com>
2022-11-29 21:01:30 +0900

    MappedSuperclass, FetchType.Lazy

---
+++ src/main/java/boardex/newboard/domain/BaseEntity.java
@@ -0,0 +1,16 @@
+package boardex.newboard.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
